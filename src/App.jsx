f75eb9a67f55615a5b979ab176ef2cc600a36ba5
zzinue <zzinue@gmail.com>
2022-08-12 22:57:16 -0500

    hoobank finished

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ const App = () =>
       <div className={`${styles.boxWidth}`}>
         <Stats />
         <Business />
+        <Billing />
         <CardDeal />
         <Testimonials />
         <Clients />
